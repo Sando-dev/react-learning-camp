e06317046f43043c1bce47df6880f9f663815c91
Bauti Corti <bautistamariacorti@gmail.com>
2024-07-29 15:57:48 -0300

    likes con useStatus agregados

--- clase 8/tarjeta-de-mascota/src/App.jsx
+++ clase 8/tarjeta-de-mascota/src/App.jsx
@@ -4,8 +4,8 @@ import Pet from './Pet';
 
 function App() {
   return (
-    <div>
-      <div className='pet-container'>
+    <div className="main-container">
+      <div className="pet-container">
         <Pet name='Jorge' owner='Martin' age='3 years' specie='dog' weight='27 kg'/>
         <Pet name='Raul' owner='Sofia' age='16 years' specie='cat' weight='15 kg'/>
         <Pet name='Carlos' owner='Ramiro' age='1 years' specie='tortoise' weight='3 kg'/>
@@ -16,7 +16,7 @@ function App() {
         <Pet name='Horacio' owner='Pedro' age='0.5 years' specie='hamster' weight='80 g'/>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- clase 8/tarjeta-de-mascota/src/Pet.jsx
+++ clase 8/tarjeta-de-mascota/src/Pet.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import './Pet.css'
 
-
 function Pet(props) {
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
+
     return (
         <div className="pet-card">
             <p className="pet-title">{props.name}'s Pet Card!</p>
-            <div></div>
             <p>Owner: {props.owner}</p>
             <p>Age: {props.age}</p>
             <p>Specie: {props.specie}</p>
             <p>Weight: {props.weight}</p>
+            <div className="button-container">
+                <button className="button-like" onClick={() => setLikes(likes + 1)}>
+                    Like {likes}
+                </button>
+                <button className="button-dislike" onClick={() => setDislikes(dislikes + 1)}>
+                    Dislike {dislikes}
+                </button>
+            </div>
         </div>
     );
 }
 
-export default Pet
+export default Pet;
